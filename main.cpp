d35f1f194456ff58d8b5f122556e43489b14e928
Saul Moreno Abril <sam6.13.93@hotmail.com>
2018-07-03 19:57:57 +0200

    Create simple Server & Client

--- main.cpp
+++ main.cpp
@@ -12,10 +12,18 @@
 #include "classes/UDPSocket.h"
 #include "classes/SocketAddress.h"
 
+#define BUFFER_SIZE 1024
+
 enum Mode : int8_t {
     ModeServer = 0,
     ModeClient = 1
 };
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_SUCCESS);
+}
 
 int main(int argc , char *argv[]) {
     Mode mode = ModeServer;
@@ -40,23 +48,142 @@ int main(int argc , char *argv[]) {
     }
     mode = static_cast<Mode>(input);
 
+
+
     if (mode == ModeServer)
     {
-        UDPSocketPtr udpSocketPtr = UDPSocket::CreateUDPSocket(INET);
+        int sockfd, newsockfd, portno;
+        socklen_t clilen;
+        char buffer[BUFFER_SIZE];
+        struct sockaddr_in serv_addr, cli_addr;
+        int n;
 
-        SocketAddressPtr addressPtr = SocketAddress::CreateIPv4FromString("127.0.0.1:7000");
-        udpSocketPtr->bindTo(*addressPtr.get());
+        std::cout << "\nInsert the port:" << std::endl;
+        std::cin >> portno;
+        std::cout << "Port:" << portno << std::endl;
+
+        // create a socket
+        // socket(int domain, int type, int protocol)
+        sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0)
+            error("ERROR opening socket");
+
+        // clear address structure
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+
+        /* setup the host_addr structure for use in bind call */
+        // server byte order
+        serv_addr.sin_family = AF_INET;
+
+        // automatically be filled with current host's IP address
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+        // convert short integer value for port must be converted into network byte order
+        serv_addr.sin_port = htons(portno);
+
+        // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
+        // bind() passes file descriptor, the address structure,
+        // and the length of the address structure
+        // This bind() call will bind  the socket to the current IP address on port, portno
+        if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+            error("ERROR on binding");
+
+        // This listen() call tells the socket to listen to the incoming connections.
+        // The listen() function places all incoming connection into a backlog queue
+        // until accept() call accepts the connection.
+        // Here, we set the maximum size for the backlog queue to 5.
+        listen(sockfd, 5);
+
+        // The accept() call actually accepts an incoming connection
+        clilen = sizeof(cli_addr);
+
+        // This accept() function will write the connecting client's address info
+        // into the the address structure and the size of that structure is clilen.
+        // The accept() returns a new socket file descriptor for the accepted connection.
+        // So, the original socket file descriptor can continue to be used
+        // for accepting new connections while the new socker file descriptor is used for
+        // communicating with the connected client.
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("ERROR on accept");
+
+        printf("Server: got connection from %s port %d\n",
+               inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+
+
+        // This send() function sends the 13 bytes of the string to the new socket
+        send(newsockfd, "Hello, world!\n", 13, 0);
+
+        bzero(buffer,256);
+
+        n = read(newsockfd,buffer,255);
+        if (n < 0) error("ERROR reading from socket");
+        printf("Here is the message: %s\n",buffer);
+
+        close(newsockfd);
+        close(sockfd);
+        return EXIT_SUCCESS;
     }
     else
     {
+        int sockfd, portno, n;
+        char hostname[BUFFER_SIZE];
+        struct sockaddr_in serv_addr;
+        struct hostent *server;
+        char buffer[BUFFER_SIZE];
 
+        std::cout << "\nInsert the hostname:" << std::endl;
+        std::cin >> hostname;
+        std::cout << "Hostname:" << hostname << std::endl;
+
+        std::cout << "\nInsert the port:" << std::endl;
+        std::cin >> portno;
+        std::cout << "Port:" << portno << std::endl;
+        fflush(stdin);
+
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        if (sockfd < 0)
+            error("ERROR opening socket");
+
+        server = gethostbyname(hostname);
+        if (server == NULL) {
+            fprintf(stderr,"ERROR, no such host\n");
+            exit(0);
+        }
+
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+        serv_addr.sin_port = htons(portno);
+
+        if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+            error("ERROR connecting");
+
+        printf("Please enter the message: ");
+        //bzero(buffer, BUFFER_SIZE);
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        fgets(buffer,BUFFER_SIZE, stdin);
+        //Remove \n character at the end of the string
+        //buffer[strlen(buffer) - 1] = 0;
+
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+            error("ERROR writing to socket");
+
+        bzero(buffer,256);
+        n = read(sockfd, buffer, 255);
+
+        if (n < 0)
+            error("ERROR reading from socket");
+
+        printf("%s\n", buffer);
+
+        close(sockfd);
+        return 0;
     }
 
-    {
-
-
-
-    }
 
     getchar();
     return 0;
