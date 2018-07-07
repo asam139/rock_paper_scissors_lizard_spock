0191233570bd554e1e759d6023a36b4a6eed837d
Saul Moreno Abril <93sauu@gmail.com>
2018-07-07 10:12:20 +0200

    Use my own class to implement the simple server

--- main.cpp
+++ main.cpp
@@ -49,54 +49,36 @@ int main(int argc , char *argv[]) {
     }
     mode = static_cast<Mode>(input);
 
-
-
     if (mode == ModeServer)
     {
-        int sockfd, newsockfd, portno;
-        socklen_t clilen;
+        int portno;
         char buffer[BUFFER_SIZE];
-        struct sockaddr_in serv_addr, cli_addr;
-        int n;
 
         std::cout << "\nInsert the port:" << std::endl;
         std::cin >> portno;
         std::cout << "Port:" << portno << std::endl;
 
-        // create a socket
-        // socket(int domain, int type, int protocol)
-        sockfd =  socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0)
-            error("ERROR opening socket");
+        SocketAddress *serverSocketAddress = new SocketAddress(INADDR_ANY, portno);
+        std::unique_ptr<SocketAddress> serverSocketAddress_ptr (serverSocketAddress);
 
-        // clear address structure
-        bzero((char *) &serv_addr, sizeof(serv_addr));
+        TCPSocketPtr serverTCPSocketPtr = TCPSocket::CreateTCPSocket(INET);
+        if (serverTCPSocketPtr == nullptr) {
+            error("ERROR creating socket");
+        }
 
-        /* setup the host_addr structure for use in bind call */
-        // server byte order
-        serv_addr.sin_family = AF_INET;
-
-        // automatically be filled with current host's IP address
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-        // convert short integer value for port must be converted into network byte order
-        serv_addr.sin_port = htons(portno);
-
-        // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
-        // bind() passes file descriptor, the address structure,
-        // and the length of the address structure
-        // This bind() call will bind  the socket to the current IP address on port, portno
-        if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        if (serverTCPSocketPtr->bindTo(*serverSocketAddress_ptr) < 0) {
             error("ERROR on binding");
+        }
 
-        // This listen() call tells the socket to listen to the incoming connections.
-        // The listen() function places all incoming connection into a backlog queue
+        // This listenTo() call tells the socket to listen to the incoming connections.
+        // The listenTo() function places all incoming connection into a backlog queue
         // until accept() call accepts the connection.
         // Here, we set the maximum size for the backlog queue to 5.
-        listen(sockfd, 5);
+        serverTCPSocketPtr->listenTo(5);
 
         // The accept() call actually accepts an incoming connection
-        clilen = sizeof(cli_addr);
+        SocketAddress *clientSocketAddress = new SocketAddress();
+        std::unique_ptr<SocketAddress> clientSocketAddress_ptr (clientSocketAddress);
 
         // This accept() function will write the connecting client's address info
         // into the the address structure and the size of that structure is clilen.
@@ -104,25 +86,27 @@ int main(int argc , char *argv[]) {
         // So, the original socket file descriptor can continue to be used
         // for accepting new connections while the new socker file descriptor is used for
         // communicating with the connected client.
-        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0)
-            error("ERROR on accept");
+        TCPSocketPtr clientTCPSocketPtr = serverTCPSocketPtr->acceptCon(*clientSocketAddress_ptr);
+        if (clientTCPSocketPtr == nullptr) {
+            error("ERROR accepting connection");
+        }
 
         printf("Server: got connection from %s port %d\n",
-               inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+               inet_ntoa(clientSocketAddress_ptr->getAsSockAddrIn()->sin_addr),
+               ntohs(clientSocketAddress_ptr->getAsSockAddrIn()->sin_port));
 
 
         // This send() function sends the 13 bytes of the string to the new socket
-        send(newsockfd, "Hello, world!\n", 13, 0);
+        if (clientTCPSocketPtr->sendTo("Hello, world!\n", 13) < 0) {
+            error("ERROR sending data");
+        }
 
-        bzero(buffer,256);
-
-        n = read(newsockfd,buffer,255);
-        if (n < 0) error("ERROR reading from socket");
+        bzero(buffer, strlen(buffer));
+        if (clientTCPSocketPtr->receiveFrom(buffer, BUFFER_SIZE) <= 0) {
+            error("ERROR receiving data");
+        }
         printf("Here is the message: %s\n",buffer);
 
-        close(newsockfd);
-        close(sockfd);
         return EXIT_SUCCESS;
     }
     else
